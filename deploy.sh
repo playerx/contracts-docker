e688f13705844246aa6586e4b518e7393860b03d
Ezeki Zibzibadze <ez@jok.io>
2018-07-15 11:04:37 +0400

    updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 rootFolderName=contracts-docker
 
+# Create neessary folders
+if [ -e /neo4j ]; then
+  echo "/neo4j folder already exists, skipped writing";
+else
+  mkdir /neo4j
+  cd /neo4j
+  mkdir /data
+  mkdir /plugins
+  cd /plugins
+  wget https://github.com/neo4j-graphql/neo4j-graphql/releases/download/3.4.0.1/neo4j-graphql-3.4.0.1.jar
+fi
+
+# Start deployment
 cd /tmp
 rm -fr $rootFolderName
 
